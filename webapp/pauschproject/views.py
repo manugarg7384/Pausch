c7f85398f41f4de41ac08497a9d19f23cbaee0b5
Manu Garg <manug@andrew.cmu.edu>
2015-04-07 18:10:51 -0400

    Server interaction possible

--- webapp/pauschproject/views.py
+++ webapp/pauschproject/views.py
@@ -1,3 +1,6 @@
+import requests
+import json
+
 from django.shortcuts import render
 
 from django.core.context_processors import csrf
@@ -5,4 +8,7 @@ from django.core.context_processors import csrf
 def home(request):
     context = {}
     context.update(csrf(request))
+    r = requests.post("http://pbridge.adm.cs.cmu.edu:11111/echo",
+                       data=json.dumps({"hello" : "hello"}))
+    print(r.text)
     return render(request, 'Home.html', context)
