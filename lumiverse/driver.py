1787f8202910967e0888e8bd308062356d41462f
Jeffrey Zhang <caretcaret@gmail.com>
2015-04-14 16:48:03 -0400

    Fix communication

--- lumiverse/driver.py
+++ lumiverse/driver.py
@@ -10,17 +10,23 @@ import lumiversepython as L
 from bottle import route, post, request
 from bottle import run as run_bottle
 
+import datetime
+import json
+
 DEFAULT_RIG_PATH = '/home/teacher/Lumiverse/PBridge.rig.json'
 
 def init_lumiverse(rig_path=DEFAULT_RIG_PATH):
     rig = L.Rig(rig_path)
     rig.init()
     rig.run()
+    rig.select('$panel=31').setColorRGBRaw("color", 1, 0, 0)
 
 @route('/echo')
 @post('/echo')
 def server_echo():
-    return request.json
+    """Echos request body with current time for testing."""
+    params = json.load(request.body)
+    return str(datetime.datetime.now()) + " " + str(params)
 
 def run_bridge_server(port=11111, handler=None):
     run_bottle(host='0.0.0.0', port=port)
