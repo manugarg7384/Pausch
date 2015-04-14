57ffd3c13e9cb74998c553c7023ba15050aa1c0f
Manu Garg <manug@andrew.cmu.edu>
2015-04-14 17:39:13 -0400

    Changed views.py

--- webapp/pauschproject/views.py
+++ webapp/pauschproject/views.py
@@ -8,7 +8,13 @@ from django.core.context_processors import csrf
 def home(request):
     context = {}
     context.update(csrf(request))
-    r = requests.post("http://pbridge.adm.cs.cmu.edu:11111/echo",
-                       data=json.dumps({"hello" : "hello"}))
+    r = requests.post("http://pbridge.adm.cs.cmu.edu:11111/update",
+                       data=json.dumps({
+                           "data": [
+                               {
+                                   "selector": "$panel=31",
+                                   "rgb": [1, 0, 0]
+                               }
+                                    ]}))
     print(r.text)
     return render(request, 'Home.html', context)
