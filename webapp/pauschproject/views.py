c744d142c5dd6f2cd0589495b1fe6fac42968c62
Manu Garg <manug@andrew.cmu.edu>
2015-04-26 21:00:06 -0400

    Added pseudo code for bridge functionality after reaching win state

--- webapp/pauschproject/views.py
+++ webapp/pauschproject/views.py
@@ -5,14 +5,9 @@ from lumiverse.io import *
 
 from random import random, randint
 
+import time
+
 def home(request):
-    while False:
-        update = BridgeUpdate()
-        panel = BridgePanel(randint(9, 49), [random(), random(), random()])
-        update.addPanel(panel)
-
-        #sendUpdate(update)
-
     return render(request, 'Home.html')
 
 def panel_input(request):
@@ -26,4 +21,17 @@ def panel_input(request):
 
 def get_bridge_state(request):
     r = requests.post(URL)
+    '''
+    if(str(r.content).count(' ') == 0): #If all the lights are on
+        timeout = time.time() + 60*5 #Play random colors for 5 minutes
+        while True:
+            update = BridgeUpdate()
+            panel = BridgePanel(randint(9, 49), [random(), random(), random()])
+            update.addPanel(panel)
+            sendUpdate(update)
+            
+            if time.time() > timeout:
+                # TO DO: reload the bridge
+                break
+    '''
     return JsonResponse(r.content)
